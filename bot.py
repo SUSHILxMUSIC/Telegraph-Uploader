1f7d3fbd5872508e8616506eea2d27212bce7df7
Mrunal <80758264+msy1717@users.noreply.github.com>
2021-10-15 13:09:39 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,6 +11,7 @@ from telethon import Button, TelegramClient, events
 from PIL import Image
 from datetime import datetime
 from telegraph import Telegraph, upload_file, exceptions
+mdnoor = "SHASA"
 TMP_DOWNLOAD_DIRECTORY = "./"
 telegraph = Telegraph()
 r = telegraph.create_account(short_name=mdnoor)
